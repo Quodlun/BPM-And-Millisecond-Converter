04d89236f248d8e765b941719077249f1e8eef5d
Mirabilis <willyfu123@gmail.com>
2022-10-19 01:19:43 +0800

    Create file main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int Mode()
+{
+    printf("Mode select:\n");
+    printf("1: BPM to milliseconds.\n");
+    printf("2: Exit.\n");
+    printf("Please select the mode: ");
+}
+
+int main()
+{
+    int mode;
+    float bpm = 0, ms = 0;
+
+    while (Mode())
+    {
+        cin >> mode;
+
+        if (mode == 0)
+        {
+            return 0;
+        }
+
+        else if (mode == 1)
+        {
+            bpm = 0;
+
+            while (cin >> bpm);
+            {
+                printf("BPM: ");
+
+                ms = 0;
+                ms = 60.0 / bpm;
+
+                printf("%f ms", ms);
+            }
+        }
+
+        else
+        {
+            printf("Mode doesn't exist.\n");
+        }
+    }
+
+    return 0;
+}
